a07ba3b06f4083dcbc2796fcd28df67816dfd410
ARMOURIT21 <ArmourIT21@gcc.edu>
2022-03-17 10:55:39 -0400

    Added a Ctor test and an IllegalArgument test for the Scores Ctor

--- src/ScoresTest.java
+++ src/ScoresTest.java
@@ -7,8 +7,22 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 public class ScoresTest {
     /*
      * Constructor tests using numScores() and get()
+     *
      */
-    
+    @Test public void ScoreCTOR(){
+        //test that the constructor can take actual whitespace-delimited integers
+        Scores sc = new Scores("1 2 3 4 5 6 7 8 9");
+        assertEquals(sc.getNumScores(),9);
+    }
+
+    @Test public void ScoreChar(){
+        Scores sc = new Scores("1 2 3 4 5 A 6 7 8 9");
+        assertThrows(IllegalArgumentException.class, ()->{});
+    }
+
+
+
+
     /*
      * getMax() tests
      */
