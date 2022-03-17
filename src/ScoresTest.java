93876af235fee883fe36577bdf396c10ac2f339d
ARMOURIT21 <ArmourIT21@gcc.edu>
2022-03-17 11:12:07 -0400

    Added emptyCTOR test, tooBig test, and noScores test. Fixed faulty use of assertEquals.

--- src/ScoresTest.java
+++ src/ScoresTest.java
@@ -12,12 +12,29 @@ public class ScoresTest {
     @Test public void ScoreCTOR(){
         //test that the constructor can take actual whitespace-delimited integers
         Scores sc = new Scores("1 2 3 4 5 6 7 8 9");
-        assertEquals(sc.getNumScores(),9);
+        assertEquals(9,sc.getNumScores());
     }
 
     @Test public void ScoreChar(){
         Scores sc = new Scores("1 2 A 3 4 5 6 7 8 9");
         assertThrows(IllegalArgumentException.class, ()->{});
+    }
+
+    @Test public void emptyCTOR(){
+        Scores sc = new Scores(null);
+        assertThrows(IllegalArgumentException.class, ()->{});
+    }
+
+    @Test public void tooBig(){
+        Scores sc = new Scores("99999999999999999999999999999999999999");
+        assertThrows(IllegalArgumentException.class, ()->{});
+    }
+
+    @Test public void noScores(){
+        Scores sc = new Scores("");
+        assertEquals(0,sc.getNumScores());
+        sc.get(0);
+        assertThrows(IndexOutOfBoundsException.class, ()->{});
     }
 
 
