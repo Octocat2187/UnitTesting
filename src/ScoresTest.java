ff044ab7b267724ebc570743a48dd43b44303835
ARMOURIT21 <ArmourIT21@gcc.edu>
2022-03-18 14:51:30 -0400

    fixed use of assertThrows

--- src/ScoresTest.java
+++ src/ScoresTest.java
@@ -16,26 +16,31 @@ public class ScoresTest {
     }
 
     @Test public void ScoreChar(){
+        //test what happens when a char is put in there
         assertThrows(IllegalArgumentException.class, ()->{
             Scores sc = new Scores("1 2 A 3 4 5 6 7 8 9");
         });
     }
 
     @Test public void emptyCTOR(){
+        //test a null constructor
         assertThrows(IllegalArgumentException.class, ()->{
             Scores sc = new Scores(null);
         });
     }
 
     @Test public void tooBig(){
+        //test not an int
         assertThrows(IllegalArgumentException.class, ()->{
             Scores sc = new Scores("99999999999999999999999999999999999999");
         });
     }
 
     @Test public void noScores(){
+        //test an empty string
         Scores sc = new Scores("");
         assertEquals(0,sc.getNumScores());
+        //get an IndexOutOfBounds
         assertThrows(IndexOutOfBoundsException.class, ()->{
             sc.get(0);
         });
