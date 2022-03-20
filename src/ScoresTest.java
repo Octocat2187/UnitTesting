ea6950dd0b077bbea653579909ddf03a8ff0febf
ARMOURIT21 <ArmourIT21@gcc.edu>
2022-03-20 18:32:49 -0400

    Tidied up code: Declared Scores sc once so that it won't need to be remade every test. Removed curly braces on AssertThrows because it kept being suggested.

--- src/ScoresTest.java
+++ src/ScoresTest.java
@@ -1,10 +1,11 @@
-import java.util.NoSuchElementException;
+//import java.util.NoSuchElementException;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class ScoresTest {
+    Scores sc;
     /*
      * Constructor tests using numScores() and get()
      *
@@ -15,7 +16,7 @@ public class ScoresTest {
      */
     @Test public void ScoreCTOR(){
         //test that the constructor can take actual whitespace-delimited integers
-        Scores sc = new Scores("1 2 3 4 5 6 7 8 9");
+        sc = new Scores("1 2 3 4 5 6 7 8 9");
         assertEquals(9,sc.getNumScores());
     }
 
@@ -24,9 +25,8 @@ public class ScoresTest {
      */
     @Test public void ScoreChar(){
         //test what happens when a char is put in there
-        assertThrows(IllegalArgumentException.class, ()->{
-            Scores sc = new Scores("1 2 X 3 4 5 6 7 8 9");
-        });
+        assertThrows(IllegalArgumentException.class, ()->
+                sc = new Scores("1 2 X 3 4 5 6 7 8 9"));
     }
 
     /**
@@ -34,9 +34,8 @@ public class ScoresTest {
      */
     @Test public void emptyCTOR(){
         //test a null constructor
-        assertThrows(IllegalArgumentException.class, ()->{
-            Scores sc = new Scores(null);
-        });
+        assertThrows(IllegalArgumentException.class, ()->
+                sc = new Scores(null));
     }
 
     /**
@@ -44,9 +43,8 @@ public class ScoresTest {
      */
     @Test public void tooBig(){
         //test not an int
-        assertThrows(IllegalArgumentException.class, ()->{
-            Scores sc = new Scores("99999999999999999999999999999999999999");
-        });
+        assertThrows(IllegalArgumentException.class, ()->
+                sc = new Scores("99999999999999999999999999999999999999"));
     }
 
     /**
@@ -54,7 +52,7 @@ public class ScoresTest {
      */
     @Test public void noScores(){
         //test an empty string
-        Scores sc = new Scores("");
+        sc = new Scores("");
         assertEquals(0,sc.getNumScores());
     }
 
@@ -62,11 +60,10 @@ public class ScoresTest {
      * .gets an index out of bounds for an empty CTOR
      */
     @Test public void OOB(){
-        Scores sc = new Scores("");
+        sc = new Scores("");
         //get an IndexOutOfBounds
-        assertThrows(IndexOutOfBoundsException.class, ()->{
-            sc.get(0);
-        });
+        assertThrows(IndexOutOfBoundsException.class, ()->
+                sc.get(0));
     }
 
 
