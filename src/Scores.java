a6decccacab23c109e3c1ab92033a1f5069add7f
ARMOURIT21 <ArmourIT21@gcc.edu>
2022-03-20 21:23:29 -0400

    Implemented a version of Scores.java for better understanding of what the code should do.

--- src/Scores.java
+++ src/Scores.java
@@ -12,22 +12,34 @@ public class Scores {
      */
     public Scores(String numbers) throws IllegalArgumentException {
         nums = new ArrayList<>();
+        Scanner parse = new Scanner(numbers);
+        while(parse.hasNextInt()){
+            nums.add(parse.nextInt());
+        }
     }
 
     /**
      * @return the number of scores in this object i.e. the number of integers in the String given to the constructor
      */
-    public int getNumScores() { return -1; }
+    public int getNumScores() { return nums.size(); }
 
     /**
      * @param index An integer index in the range [0, getNumScores())
      * @return The score at the specified index. For example, the first score in the String is at index 0.
      */
-    public int get(int index) { return -1; }
+    public int get(int index) { return nums.get(index); }
 
     /**
      * @return the maximum score stored in this object
      * @throws java.util.NoSuchElementException if there are no scores
      */
-    public int getMax() throws NoSuchElementException { return -1; }
+    public int getMax() throws NoSuchElementException {
+        int max = Integer.MIN_VALUE;
+        for (Integer num : nums) {
+            if (max < num) {
+                max = num;
+            }
+        }
+        return max;
+    }
 }
